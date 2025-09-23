d542b12f4b68b3e8231116a2d7749e75a5aa3e22
gschubert05 <gabrielschubert05@gmail.com>
2025-09-23 15:14:56 +1000

    scraper test

--- scraper/test-scraper.py
+++ scraper/test-scraper.py
@@ -1,31 +1,48 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-def main():
-    chrome_opts = Options()
-    chrome_opts.add_argument("--no-sandbox")
-    chrome_opts.add_argument("--disable-dev-shm-usage")
-    chrome_opts.add_argument("--disable-gpu")
+TARGET_URL = "http://odds.aussportsbetting.com/multibet"
 
-    # Run in headed mode under Xvfb
-    driver = webdriver.Chrome(options=chrome_opts)
+def make_driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1600,1200")
+    return webdriver.Chrome(options=options)
 
+def test_compid(compid: int):
+    driver = make_driver()
     try:
-        url = "http://odds.aussportsbetting.com/multibet"
-        driver.get(url)
-        print("Page title is:", driver.title)
+        driver.get(TARGET_URL)
+        print("Page loaded:", driver.title)
 
-        # Try grab the H1 heading if it exists
-        try:
-            h1 = driver.find_element(By.TAG_NAME, "h1")
-            print("Found H1 text:", h1.text)
-        except Exception as e:
-            print("Could not find H1 element:", e)
+        # Find the compid input
+        input_el = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, "compid"))
+        )
+
+        # Set compid
+        driver.execute_script("arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('change', {bubbles:true}));", input_el, compid)
+        print(f"Set compid to {compid}")
+
+        # Click the update button
+        update_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "update"))
+        )
+        update_btn.click()
+        print("Clicked update button")
+
+        # Wait for some result element to show up
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "more-market-odds"))
+        )
+        print("Page updated, compid selection works!")
 
     finally:
         driver.quit()
 
 if __name__ == "__main__":
-    main()
+    test_compid(11)  # example compid
